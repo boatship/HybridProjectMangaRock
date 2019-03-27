1d09b5491399f090e900823b989b94b25242f2f6
boatship <boatata@hotmail.com>
2019-03-27 23:24:24 +0700

    init

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createStackNavigator, createAppContainer , createBottomTabNavigator } from 'react-navigation';
+import Discover from '../screens/Discover';
+import Favorites from '../screens/Favorites';
+import Recent from '../screens/Recent';
+import Downloads from '../screens/Downloads';
+import More from '../screens/More';
+
+const AppNavigator = createStackNavigator(
+  {
+    DISCOVER: {
+      screen: Discover
+    },
+    FAVORITES: {
+      screen: Favorites
+    },
+    RECENT: {
+      screen: Recent
+    },
+    DOWNLOADS: {
+      screen: Downloads
+    },
+    MORE: {
+      screen: More
+    }
+  },
+  {
+    initialRouteName: "DISCOVER"
+  }
+);
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    DISCOVER: {
+      screen: Discover
+    },
+    FAVORITES: {
+      screen: Favorites
+    },
+    RECENT: {
+      screen: Recent
+    },
+    DOWNLOADS: {
+      screen: Downloads
+    },
+    MORE: {
+      screen: More
+    }
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'DISCOVER') {
+          iconName = `md-compass`;
+        } else if (routeName === 'FAVORITES') {
+          iconName = `md-heart`;
+        } else if (routeName === 'RECENT') {
+          iconName = `md-time`;
+        } else if (routeName === 'DOWNLOADS') {
+          iconName = `md-download`;
+        } else if (routeName === 'MORE') {
+          iconName = `ios-more`;
+        }
+
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={20} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: '#45B4FF',
+      inactiveTintColor: '#909090',
+    },
+  }
+);
+
+
+const AppContainer = createAppContainer(TabNavigator);
+
+export default AppContainer;
---
+++ screens/Discover.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { View, Text, SafeAreaView, StyleSheet, ScrollView} from 'react-native';
+import {Header} from 'react-native-elements';
+import MaterialTabs from 'react-native-material-tabs';
+
+export default class Discover extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  state = {
+    selectedTab: 0,
+  };
+ 
+  setTab = selectedTab => {
+    this.setState({ selectedTab });
+  };
+
+  render() {
+    return (
+      <View>
+        
+        <Header
+          placement="left"
+          backgroundColor="white"
+          leftComponent={{ text: 'Manga Rock', style: { fontSize:20,color: 'black' } }}
+          centerComponent={{icon : 'arrow-drop-down', color: 'black'}}
+          rightComponent={{ icon: 'search', color: 'black' }}
+        />
+        
+        <SafeAreaView style={styles.container}>
+        <MaterialTabs
+          items={['One', 'Two', 'Three', 'Four', 'Five']}
+          selectedIndex={this.state.selectedTab}
+          onChange={this.setTab}
+          barColor="#1fbcd2"
+          indicatorColor="#fffe94"
+          activeTextColor="white"
+        />
+      </SafeAreaView>
+      <ScrollView>
+      <Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text><Text>48484</Text>
+      </ScrollView>
+      </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});

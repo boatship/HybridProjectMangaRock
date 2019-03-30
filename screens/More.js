2463df90ff45fa4b7079f983c5e95881882523ea
DurianBeta <38138611+DurianBeta@users.noreply.github.com>
2019-03-30 21:50:16 +0700

    Update More.js

--- screens/More.js
+++ screens/More.js
@@ -1,17 +1,33 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, SafeAreaView, StyleSheet, ScrollView,Flex } from "react-native";
+import { Header } from "react-native-elements";
+import { Avatar } from "react-native-elements";
 
 export default class More extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
   render() {
     return (
       <View>
-        <Text> More </Text>
+        <Header
+          placement="left"
+          backgroundColor="white"
+          leftComponent={{
+            text: "More",
+            style: { fontSize: 20, color: "black" }
+          }}
+          centerComponent={{}}
+          rightComponent={{ icon: "bell", color: "black", type: "font-awesome" }}
+        />
+        <div>
+          <Avatar
+            rounded
+            icon={{ name: "user", type: "font-awesome" }}
+          /> <text></text>
+        </div>
       </View>
     );
   }

6a3e49078ff86ed08bddb38cb9edddf38cffa7c3
DurianBeta <38138611+DurianBeta@users.noreply.github.com>
2019-03-31 01:24:10 +0700

    Update More.js

--- screens/More.js
+++ screens/More.js
@@ -5,7 +5,8 @@ import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
-  List
+  List,
+  Flex
 } from "react-native";
 import { Header, Avatar, ListItem } from "react-native-elements";
 import MaterialTabs from "react-native-material-tabs";
@@ -21,82 +22,80 @@ export default class More extends Component {
     return (
       <View>
         <ScrollView>
-        <Header
-          placement="left"
-          backgroundColor="white"
-          leftComponent={{
-            text: "More",
-            style: { fontSize: 20, color: "black" }
-          }}
-          centerComponent={{}}
-          rightComponent={{
-            icon: "bell",
-            color: "black",
-            type: "font-awesome"
-          }}
-        />
-        <SafeAreaView style={styles.container} />
-        <View>
-          <Flex>
-          <Avatar
-            size="medium"
-            rounded
-            icon={{ name: "user", type: "font-awesome" }}
-            containerStyle={{ marginLeft: 20, marginTop: 20 }}
+          <Header
+            placement="left"
+            backgroundColor="white"
+            leftComponent={{
+              text: "More",
+              style: { fontSize: 20, color: "black" }
+            }}
+            centerComponent={{}}
+            rightComponent={{
+              icon: "bell",
+              color: "black",
+              type: "font-awesome"
+            }}
           />
-          </Flex>
-        </View>
-        <View>
-          <Text> Configuration </Text>
-          {list.map((item, i) => (
-            <ListItem
-              key={i}
-              title={item.title}
-              leftIcon={{ name: item.icon }}
-            />
-          ))}
-        </View>
-        <View>
-          <Text> Upgrade </Text>
-          {list2.map((item, i) => (
-            <ListItem
-              key={i}
-              title={item.title}
-              leftIcon={{ name: item.icon }}
-            />
-          ))}
-        </View>
-        <View>
-          <Text> Add-ons </Text>
-          {list3.map((item, i) => (
-            <ListItem
-              key={i}
-              title={item.title}
-              leftIcon={{ name: item.icon }}
-            />
-          ))}
-        </View>
-        <View>
-          <Text> Support </Text>
-          {list4.map((item, i) => (
-            <ListItem
-              key={i}
-              title={item.title}
-              leftIcon={{ name: item.icon }}
-            />
-          ))}
-        </View>
-        <View>
-          <Text> General </Text>
-          {list5.map((item, i) => (
-            <ListItem
-              key={i}
-              title={item.title}
-              leftIcon={{ name: item.icon }}
-              subtitle={item.subtitle}
-            />
-          ))}
-        </View>
+          <SafeAreaView style={styles.container} />
+          <View style={styles.container}>
+                <Avatar
+                  size="medium"
+                  rounded
+                  icon={{ name: "user", type: "font-awesome" }}
+                  containerStyle={{ marginLeft: 20, marginTop: 10 }}
+                />
+          </View>
+          <View style={styles.container}>
+            <Text style={styles.textcolor}> Configuration </Text>
+            {list.map((item, i) => (
+              <ListItem
+                key={i}
+                title={item.title}
+                leftIcon={{ name: item.icon }}
+              />
+            ))}
+          </View>
+          <View style={styles.container}>
+            <Text style={styles.textcolor}> Upgrade </Text>
+            {list2.map((item, i) => (
+              <ListItem
+                key={i}
+                title={item.title}
+                leftIcon={{ name: item.icon }}
+              />
+            ))}
+          </View>
+          <View style={styles.container}>
+            <Text style={styles.textcolor}> Add-ons </Text>
+            {list3.map((item, i) => (
+              <ListItem
+                key={i}
+                title={item.title}
+                leftIcon={{ name: item.icon }}
+              />
+            ))}
+          </View>
+          <View style={styles.container}>
+            <Text style={styles.textcolor}> Support </Text>
+            {list4.map((item, i) => (
+              <ListItem
+                key={i}
+                title={item.title}
+                leftIcon={{ name: item.icon }}
+              />
+            ))}
+          </View>
+          <View style={styles.container}>
+            <Text style={styles.textcolor}> General </Text>
+            {list5.map((item, i) => (
+              <ListItem
+                key={i}
+                title={item.title}
+                leftIcon={{ name: item.icon }}
+                subtitle={item.subtitle}
+              />
+            ))}
+          </View>
         </ScrollView>
       </View>
     );
@@ -105,8 +104,13 @@ export default class More extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  }
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da'
+  },
+  textcolor: {
+    color: 'gray',
+  },
 });
 
 const list = [
@@ -147,10 +151,10 @@ const list4 = [
 const list5 = [
   {
     title: "Version",
-    subtitle: '3.8.4_definitive'
+    subtitle: "3.8.4_definitive"
   },
   {
     title: "Check for Updates"
   },
-  {title: "About Us"}
+  { title: "About Us" }
 ];

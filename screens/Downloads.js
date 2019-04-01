2c244341d63647badc852fd381581fb545fa2855
DurianBeta <38138611+DurianBeta@users.noreply.github.com>
2019-04-02 01:31:25 +0700

    Update Downloads.js

--- screens/Downloads.js
+++ screens/Downloads.js
@@ -1,33 +1,63 @@
-import React, { Component } from 'react';
-import { View, Text, SafeAreaView, StyleSheet, ScrollView } from 'react-native';
-import { Header } from 'react-native-elements';
-
+import React, { Component } from "react";
+import { View, Text, SafeAreaView, StyleSheet, ScrollView } from "react-native";
+import { Header, ListItem } from "react-native-elements";
+import Download2 from "../components/Download/Download.js";
 export default class Downloads extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
   render() {
     return (
       <View>
         <ScrollView>
-        <Header
-          placement="right"
-          backgroundColor="white"
-          leftComponent={{ text: 'Downloads', style: { fontSize: 20, color: 'black' } }}
-          centerComponent={{ icon: 'search', color: 'black' }}
-          rightComponent={{ icon: 'more-vert', color: 'black' }}
+          <Header
+            placement="right"
+            backgroundColor="white"
+            leftComponent={{
+              text: "Downloads",
+              style: { fontSize: 20, color: "black" }
+            }}
+            centerComponent={{ icon: "search", color: "black" }}
+            rightComponent={{ icon: "more-vert", color: "black" }}
+            containerStyle={{
+              elevation: 4,
+              shadowOffset: { width: 5, height: 5 },
+              shadowColor: "grey",
+              shadowOpacity: 0.5,
+              shadowRadius: 10
+            }}
+          />
+
+          <Text
+            style={{
+              marginLeft: 20,
+              marginTop: 20,
+              fontSize: 15,
+              fontWeight: "bold"
+            }}
+          >
+            2 manga{"                                                       "}
+            <Text style={{ color: "#45B4FF", fontWeight: "bold" }}>
+              Sort by: Modi... ▾
+            </Text>
+          </Text>
+
+          <ScrollView>
+            <Download2 />
+          </ScrollView>
+        </ScrollView>
+        <ListItem
+          title={<Text style={{marginLeft:130}}>2/3 free slot(s) used ></Text>}
           containerStyle={{
-            elevation: 4,
-            shadowOffset: { width: 5, height: 5 },
-            shadowColor: "grey",
-            shadowOpacity: 0.5,
-            shadowRadius: 10,
+            height:20,
+            marginTop: 240,
+            borderRadius: 4,
+            borderWidth: 0.8,
+            borderColor: "#d6d7da"
           }}
         />
-        </ScrollView>
       </View>
     );
   }

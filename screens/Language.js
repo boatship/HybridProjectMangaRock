2b9202547151561c31943450bc2f3a09d55ac1f0
boatship <boatata@hotmail.com>
2019-04-02 00:23:52 +0700

    finished languange page

--- screens/Language.js
+++ screens/Language.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
 
 class Language extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      value:0
     };
+
+    
   }
+
+  
 
   static navigationOptions = {
     title: 'Language'
   };
 
-  render() {
+  render() { var radio_props = [
+    {label: 'English', value: 0 },
+    {label: 'Hindi', value: 1 },
+    {label: 'Deutsch', value: 2 },
+    {label: 'Espanol', value: 3 },
+    {label: 'Francais', value: 4 },
+    {label: 'Indonesia', value: 5 },
+    {label: 'Italino', value: 6 },
+    {label: 'Portugues', value: 7 },
+    {label: 'Russian', value: 8 },
+    {label: 'Turkese', value: 9 },
+    {label: 'Chinese', value: 10 },
+  ];
     return (
       <View>
-        <Text> textInComponent </Text>
+        <ScrollView>
+        <RadioForm
+          style={{marginTop: 20,marginLeft: 20}}
+          radio_props={radio_props}
+          initial={0}
+          onPress={(value) => {this.setState({value:value})}}
+        />
+        </ScrollView>
       </View>
     );
   }
